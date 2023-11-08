2eb5b9782bbcd87f788af1d6a4d079af0ea38430
phtniit <phtniit@github.com>
2023-11-09 00:20:53 +0800

    modify checker

--- checker.cc
+++ checker.cc
@@ -9,8 +9,8 @@ int main(int argc, char* argv[]) {
   int tot = atoi(argv[4]);
   system("g++ -std=c++14 -O2 -o _generator generator.cc");
   system("g++ -std=c++14 -O2 -o _brute brute.cc");
-  // system("g++ -std=c++14 -O2 -fsanitize=address -o tocheck a.cc");
-  system("g++ -std=c++14 -O2 -o tocheck a.cc");
+  system("g++ -std=c++14 -O2 -fsanitize=address -o tocheck a.cc");
+  // system("g++ -std=c++14 -O2 -o tocheck a.cc");
   for (int cas = 1; cas <= tot; ++cas) {
     system("./_generator > task.in");
     system("./_brute < task.in > task.ans");
