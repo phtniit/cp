f480202c8a0c89a9fe8fc12701a1352400263643
phtniit <phtniit@github.com>
2023-11-09 00:21:27 +0800

    fix sam

--- sam.cc
+++ sam.cc
@@ -8,8 +8,8 @@ typedef pair<int, int> pii;
 const int debug_ = 0;
 const int inf = 1e9+7;
 
-const int N = 500000 + 10;
-unordered_map<i64, int> nex[N*2];
+const int N = 1000000 + 10;
+int nex[N*2][26];
 int fal[N*2];
 int len[N*2];
 int pos[N*2];
@@ -23,11 +23,11 @@ void sam_init() {
   len[0] = 0;
   pos[0] = 0;
 }
-void sam_add(i64 c) {
+void sam_add(int c) {
   len[++cur] = len[las] + 1;
   pos[cur] = len[cur];
   int p = las;
-  while (p != -1 && nex[p].find(c) == nex[p].end()) {
+  while (p != -1 && nex[p][c] == 0) {
     nex[p][c] = cur;
     p = fal[p];
   }
@@ -45,7 +45,7 @@ void sam_add(i64 c) {
   }
 
   len[++cur] = len[p] + 1;
-  nex[cur] = nex[q];
+  for (int i = 0; i < 26; ++i) nex[cur][i] = nex[q][i];
   fal[cur] = fal[q];
   pos[cur] = -abs(pos[q]);
   while (p != -1 && nex[p][c] == q) {
@@ -55,90 +55,41 @@ void sam_add(i64 c) {
   fal[las] = fal[q] = cur;
 }
 
+int dep[N * 2], R[N * 2];
+void dfs(int p, int d) {
+  R[p] = -1;
+  for (int i = 25; i >= 0; --i) if (nex[p][i] && R[nex[p][i]] == 0) {
+    dfs(nex[p][i], d+1);
+  }
+  static int rnk = 0;
+  R[p] = ++rnk;
+  dep[rnk] = d;
+}
+vector<int> T[N * 2];
+void down(int u, int rnk) {
+  R[u] = min(R[u], rnk);
+  for (auto v: T[u]) {
+    down(v, R[u]);
+  }
+}
 int main() {
-  int n, m;
-  scanf("%d %d", &n, &m);
+  static char s[N];
+  scanf("%s", s+1);
+  int n = strlen(s+1);
 
   sam_init();
+  for (int i = 1; i <= n; ++i) sam_add(s[i]-'a');
+  dfs(0, 0);
+  for (int i = 1; i <= cur; ++i) T[fal[i]].push_back(i);
+  down(0, inf);
+
+  static int ans[N];
+  for (int i = 1; i <= cur; ++i) if (pos[i] > 0) {
+    ans[pos[i]] = dep[R[i]];
+  }
+
   for (int i = 1; i <= n; ++i) {
-    for (int j = 1, v; j <= m; ++j) {
-      scanf("%d", &v);
-      i64 u = v;
-      u <<= 32;
-      u += j;
-      sam_add(u);
-    }
+    printf("%d %d\n", i-ans[i]+1, i);
   }
-
-  static vector<int> g[N*2];
-  for (int i = 1; i <= cur; ++i) {
-    g[fal[i]].push_back(i);
-  }
-
-  static set<int> s[N*2];
-  static i64 w[N*2];
-  i64 ans = 0;
-  std::function<void(int)> dfs = [&](int u) {
-    int idx = u;
-    for (int i = 0; i < g[u].size(); ++i) {
-      int v = g[u][i];
-      dfs(v);
-      if (s[v].size() > s[idx].size()) {
-        idx = v;
-      }
-    }
-
-    if (u == 0) {
-      return;
-    }
-    if (idx != u) {
-      swap(s[idx], s[u]);
-      w[u] = w[idx];
-    } else {
-      s[u].insert(0);
-    }
-    assert(*(s[u].begin()) == 0);
-
-    auto add = [&](int r) {
-      auto it = s[u].lower_bound(r);
-      assert(it != s[u].begin());
-      int rig = (it == s[u].end() ? 0 : *it);
-      --it;
-      static i64 one = 1;
-      if (rig) {
-        w[u] -= (one + n - rig) * (r - *it);
-      }
-      w[u] += (one + n - r) * (r - *it);
-      assert(s[u].find(r) == s[u].end());
-      s[u].insert(r);
-    };
-
-    int row = (abs(pos[u]) + m - 1) / m, col = abs(pos[u]) - m * (row-1);
-    if (pos[u] > 0) {
-      add(row);
-    }
-
-    for (int i = 0; i < g[u].size(); ++i) {
-      int v = g[u][i];
-      if (v != idx) {
-        for (auto it = s[v].begin(); it != s[v].end(); ++it) {
-          if (*it > 0) {
-            add(*it);
-          }
-        }
-      }
-    }
-
-    //{lenu, ..., lenf, ... 1}
-    //{......, col, ..., 1}
-    if (col > len[fal[u]]) {
-      int d = min(len[u], col) - len[fal[u]];
-      ans += w[u] * d;
-    }
-  };
-  dfs(0);
-  cout << ans << endl;
-
   return 0;
 }
-
