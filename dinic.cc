d51b985a90ede5016c4577648de5a649f1576ef3
phtniit <phtniit@github.com>
2023-12-21 11:17:49 +0800

    rewrite dinic in class

--- dinic.cc
+++ dinic.cc
@@ -9,188 +9,141 @@ const int prm = 998244353;
 const i64 inf = 1000000007;
 const i64 inf2 = inf*inf;
 
-vector<int> g[maxn];
-int cur[maxn];
-int ee, vv[maxn], cap[maxn], flow[maxn];
-void init(int n) { // ATTENTION: TO BE CALLED
-  ee = 0;
-  for (int i = 1; i <= n; ++i) {
-    g[i].clear();
+namespace atcoder {
+
+template <class Cap> struct mf_graph {
+ public:
+  int limint;
+  Cap limT;
+  vector<vector<int>> g;
+  vector<int> vv;
+  vector<Cap> cap, flow;
+  int ee;
+
+  mf_graph(int N, const int E = 660000) {
+    limint = std::numeric_limits<int>::max();
+    limT = std::numeric_limits<Cap>::max();
+
+    g.resize(N+1);
+    vv.resize(E);
+    cap.resize(E);
+    flow.resize(E);
+    ee = 0;
   }
-}
-void adj(int u, int v, int w) {
-  cap[ee] = w;
-  flow[ee] = 0;
-  vv[ee] = v;
-  g[u].push_back(ee);
-  ++ee;
-  cap[ee] = 0;
-  flow[ee] = 0;
-  vv[ee] = u;
-  g[v].push_back(ee);
-  ++ee;
-}
 
-int dinic(int n, int S, int T) {
-  static int lev[maxn];
-
-  auto bfs = [&]() {
+  void rebuild(int n) { // ATTENTION: TO BE CALLED
+    assert(n < g.size());
+    ee = 0;
     for (int i = 1; i <= n; ++i) {
-      cur[i] = 0;
-      lev[i] = inf;
+      g[i].clear();
     }
-    static queue<int> q;
-    lev[S] = 0;
-    q.push(S);
-    while (!q.empty()) {
-      int u = q.front(); q.pop();
-      for (int i = 0; i < g[u].size(); ++i) {
-        int e = g[u][i], v = vv[e];
-        if (cap[e] > flow[e] && lev[v] == inf) {
-          lev[v] = lev[u] + 1;
-          q.push(v);
+  }
+  void adj(int u, int v, Cap w) {
+    cap[ee] = w;
+    flow[ee] = 0;
+    vv[ee] = v;
+    g[u].push_back(ee);
+    ++ee;
+    cap[ee] = 0;
+    flow[ee] = 0;
+    vv[ee] = u;
+    g[v].push_back(ee);
+    ++ee;
+  }
+
+  Cap dinic(int n, int S, int T) {
+    static vector<int> lev;
+    if (lev.size() < n+1) {
+      lev.resize(n+1);
+    }
+    static vector<int> cur;
+    if (cur.size() < n+1) {
+      cur.resize(n+1);
+    }
+
+    auto bfs = [&]() {
+      for (int i = 1; i <= n; ++i) {
+        cur[i] = 0;
+        lev[i] = limint;
+      }
+      static queue<int> q;
+      assert(q.empty());
+      lev[S] = 0;
+      q.push(S);
+      while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (int i = 0; i < g[u].size(); ++i) {
+          int e = g[u][i], v = vv[e];
+          if (cap[e] > flow[e] && lev[v] == limint) {
+            lev[v] = lev[u] + 1;
+            q.push(v);
+          }
         }
       }
-    }
-  };
+    };
 
-  std::function<int(int, int)> dfs = [&](int u, int c) {
-    if (u == T) {
-      return c;
-    }
-    int ret = 0;
-    for (int& i = cur[u]; i < g[u].size(); ++i) {
-      int e = g[u][i], v = vv[e];
-      if (cap[e] <= flow[e] || lev[v] != lev[u] + 1) {
-        continue;
+    std::function<Cap(int, Cap)> dfs = [&](int u, Cap c) {
+      if (u == T) {
+        return c;
       }
-      int tmp = dfs(v, min(c, cap[e]-flow[e]));
-      flow[e] += tmp;
-      flow[e^1] -= tmp;
-      ret += tmp;
-      c -= tmp;
-      if (c == 0) {
+      Cap ret = 0;
+      for (int& i = cur[u]; i < g[u].size(); ++i) {
+        int e = g[u][i], v = vv[e];
+        if (cap[e] <= flow[e] || lev[v] != lev[u] + 1) {
+          continue;
+        }
+        Cap tmp = dfs(v, min(c, cap[e]-flow[e]));
+        flow[e] += tmp;
+        flow[e^1] -= tmp;
+        ret += tmp;
+        c -= tmp;
+        if (c == 0) {
+          break;
+        }
+      }
+      return ret;
+    };
+
+    Cap ret = 0;
+    while (true) {
+      bfs();
+      Cap tmp = dfs(S, limT);
+      if (tmp == 0) {
         break;
       }
+      ret += tmp;
     }
     return ret;
-  };
-
-  int ret = 0;
-  while (true) {
-    bfs();
-    int tmp = dfs(S, inf);
-    if (tmp == 0) {
-      break;
-    }
-    ret += tmp;
-  }
-  return ret;
-}
-
-void paint(int n, int S, vector<int>& cuts) {
-  static int col[maxn];
-  std::function<void(int)> dfs = [&](int u) {
-    col[u] = 1;
-    for (int i = 0; i < g[u].size(); ++i) {
-      int e = g[u][i];
-      if (cap[e] > flow[e] && !col[vv[e]]) {
-        dfs(vv[e]);
-      }
-    }
-  };
-  for (int i = 1; i <= n; ++i) {
-    col[i] = 0;
-  }
-  dfs(S);
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 0; j < g[i].size(); ++j) {
-      int e = g[i][j];
-      if (cap[e] > 0 && cap[e] == flow[e] && col[i] + col[vv[e]] == 1) {
-        cuts.push_back(e);
-      }
-    }
-  }
-}
-
-int main() {
-  int n1, n2, m, u, v;
-  cin >> n1 >> n2 >> m;
-  int n = n1 + n2 + 2, s = n1 + n2 + 1, t = n1 + n2 + 2;
-
-  static int in[maxn];
-  for (int h = 0; h < m; ++h) {
-    cin >> u >> v;
-    adj(u, n1+v, 1);
-    in[u]++;
-    in[n1+v]++;
-  }
-  int d = 2000;
-  for (int i = 1; i <= n1+n2; ++i) {
-    d = min(in[i], d);
   }
 
-  puts("0");
-
-  int tot = 0;
-  for (int i = 1; i <= d; ++i) {
-    for (int i = 1; i <= n1; ++i) {
-      adj(s, i, 1);
+  void paint(int n, int S, vector<int>& cuts) {
+    static vector<int> col;
+    if (col.size() < n+1) {
+      col.resize(n+1);
     }
-    for (int i = 1; i <= n2; ++i) {
-      adj(n1+i, t, 1);
-    }
-    tot += dinic(n, s, t);
-    cout << (n1 + n2) * i - tot << endl;
-
-    static int vise[maxn];
-    for (int j = 0; j < m; ++j) {
-      vise[j] = 0;
-    }
-
-    for (int u = 1; u <= n1; ++u) {
-      int cnt = 0;
-      for (int j = 0; j < g[u].size(); ++j) if (cap[g[u][j]] == 1) {
-        int e = g[u][j];
-        assert(e % 2 == 0);
-        if (flow[e] == 1) {
-          vise[e/2] = 1;
-          cnt++;
+    std::function<void(int)> dfs = [&](int u) {
+      col[u] = 1;
+      for (int i = 0; i < g[u].size(); ++i) {
+        int e = g[u][i];
+        if (cap[e] > flow[e] && !col[vv[e]]) {
+          dfs(vv[e]);
         }
       }
-      for (int j = 0; j < g[u].size() && cnt < i; ++j) if (cap[g[u][j]] == 1) {
-        int e = g[u][j];
-        if (flow[e] == 0) {
-          vise[e/2] = 1;
-          cnt++;
+    };
+    for (int i = 1; i <= n; ++i) {
+      col[i] = 0;
+    }
+    dfs(S);
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 0; j < g[i].size(); ++j) {
+        int e = g[i][j];
+        if (cap[e] > 0 && cap[e] == flow[e] && col[i] + col[vv[e]] == 1) {
+          cuts.push_back(e);
         }
       }
-      assert(cnt >= i);
     }
-
-    for (int u = n1+1; u <= n1+n2; ++u) {
-      int cnt = 0;
-      for (int j = 0; j < g[u].size(); ++j) if (cap[g[u][j]] == 0) {
-        int e = g[u][j];
-        assert(e % 2 == 1);
-        if (vise[e/2]) {
-          cnt++;
-        }
-      }
-      for (int j = 0; j < g[u].size() && cnt < i; ++j) if (cap[g[u][j]] == 0) {
-        int e = g[u][j];
-        if (!vise[e/2]) {
-          vise[e/2] = 1;
-          cnt++;
-        }
-      }
-      assert(cnt >= i);
-    }
-    for (int j = 1; j <= m; ++j) if (vise[j-1]) {
-      printf("%d ", j);
-    }
-    puts("");
   }
-  return 0;
+};
+
 }
 
