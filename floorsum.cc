7fd5bcdb4b715ff039d81ad7ea1d4e8c4ed403e8
phtniit <phtniit@github.com>
2024-03-25 22:19:48 +0800

    add floor_sum

--- floorsum.cc
+++ floorsum.cc
@@ -13,6 +13,25 @@ const i64 prm = 998244353;
 //const i64 inf2 = inf*inf;
 const int maxn = 500010;
 
+i64 floor_sum(i64 n, i64 m, i64 a, i64 b) { // \sum{0<=i<=n-1}{[(a*i+b)/m]}
+    i64 ans = 0;
+    if (a >= m) {
+        ans += (n - 1) * n * (a / m) / 2;
+        a %= m;
+    }
+    if (b >= m) {
+        ans += n * (b / m);
+        b %= m;
+    }
+
+    i64 y_max = (a * n + b) / m, x_max = (y_max * m - b);
+    if (y_max == 0) return ans;
+    ans += (n - (x_max + a - 1) / a) * y_max;
+    ans += floor_sum(y_max, a, m, (a - x_max % a) % a);
+    return ans;
+}
+
+
 void floorRng(i64 n, vector<pll>& vt) {
   for (i64 i = 1; i <= n; ++i) {
     i64 k = n/i, r = n/k;
