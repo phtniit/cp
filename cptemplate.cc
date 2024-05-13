79c806e7fe9ef112deee66e68c22cba1ecb9d5d2
phtniit <phtniit@github.com>
2024-05-13 17:34:14 +0800

    swap cptemplate cputil

--- cptemplate.cc
+++ cptemplate.cc
@@ -1,25 +1,11 @@
-//#pragma GCC optimize("Ofast","-funroll-loops")
-//#pragma GCC target("sse4.1","sse4.2","ssse3","sse3","sse2","sse","avx2","avx","popcnt","tune=native")
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-typedef long double ldb;
 typedef long long i64;
-typedef unsigned long long u64;
-typedef unsigned int u32;
 typedef pair<int, int> pii;
 
-// std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-// std::shuffle(vt.begin(), vt.end(), std::default_random_engine(rng()));
-// priority_queue<int, vector<int>, greater<int>> minq;
-// ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-// fflush(stdout);
-
 const int inf = 1000000007;
-const i64 prm = 998244353;
-const i64 inf2 = ((i64)inf) * inf;
 const int maxn = 1100010; // 1.1e6
 
 inline int read(){
@@ -28,24 +14,6 @@ inline int read(){
   while(isdigit(ch)) x=(x<<3)+(x<<1)+(ch^48),ch=getchar();
   return f?-x:x;
 }
-
-/*
-void read(__int128 &x){
-	// read a __int128 variable
-	char c; bool f = 0;
-	while(((c = getchar()) < '0' || c > '9') && c != '-');
-	if(c == '-'){f = 1; c = getchar();}
-	x = c - '0';
-	while((c = getchar()) >= '0' && c <= '9')x = x * 10 + c - '0';
-	if(f) x = -x;
-}
-void write(__int128 x){
-	// print a __int128 variable
-	if(x < 0){putchar('-'); x = -x;}
-	if(x > 9)write(x / 10);
-	putchar(x % 10 + '0');
-}
-*/
 
 void once() {
   int n = read();
